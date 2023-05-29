37a0b7e208d9b391e76155cc86bcd0e144614f4a
SeanLy <seanly@opsbox.dev>
2023-05-29 10:36:39 +0000

    update config

--- get-plugins.sh
+++ get-plugins.sh
@@ -1,2 +1,2 @@
-JENKINS_HOST=127.0.0.1:30080
-curl -sSL "http://$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/'
+JENKINS_HOST=127.0.0.1
+curl -sSL "http
